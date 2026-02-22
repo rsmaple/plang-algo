5eaca7d5d0a41adec01dec27efff0a947cc4edc0
rsmaple <wogkdkrm@gmail.com>
2026-02-22 23:14:48 +0900

    feat: sol_3 test code 추가

--- src/sol_3/solution.test.ts
+++ src/sol_3/solution.test.ts
@@ -1,7 +1,12 @@
-import { describe, expect, it } from "vitest";
-import { solution } from "./solution";
+import { describe, expect, it } from 'vitest';
+import { solution } from './solution';
 
-describe("sol_3", () => {
-  it("Example 1", () => {
+describe('sol_3', () => {
+  it('[1,2,2,1] & [2,2] -> [2]', () => {
+    expect(solution([1, 2, 2, 1], [2, 2])).toEqual([2]);
+  });
+
+  it('[4,9,5] & [9,4,9,8,4] -> [4,9]', () => {
+    expect(solution([4, 9, 5], [9, 4, 9, 8, 4])).toEqual([4, 9]);
   });
 });
