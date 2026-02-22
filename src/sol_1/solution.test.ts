846761eb3e1b859decf3ca7e88aace6feafad64b
rsmaple <wogkdkrm@gmail.com>
2026-02-22 22:34:11 +0900

    feat: oxfmt, sol1~3 폴더 기본 세팅

---
+++ src/sol_1/solution.test.ts
@@ -0,0 +1,6 @@
+import { describe, expect, it } from "vitest";
+import { solution } from "./solution";
+
+describe("sol_1", () => {
+  it("Example 1", () => {});
+});
---
+++ src/sol_3/solution.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+import { solution } from "./solution";
+
+describe("sol_3", () => {
+  it("Example 1", () => {
+  });
+});
