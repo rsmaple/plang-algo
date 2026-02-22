d4bac346b8df75d50a4f4c23b217c9f6b2f8fffb
rsmaple <wogkdkrm@gmail.com>
2026-02-22 23:03:11 +0900

    feat: sol_3 - 두 배열의 교집합

--- src/sol_3/solution.ts
+++ src/sol_3/solution.ts
@@ -1,2 +1,10 @@
-export function solution() {
+export function solution(nums1: number[], nums2: number[]): number[] {
+  const set1 = new Set(nums1);
+  const set2 = new Set(nums2);
+
+  const intersection = [...set1].filter((num) => set2.has(num));
+  return intersection;
 }
+
+console.log(solution([1, 2, 2, 1], [2, 2])); // [2]
+console.log(solution([4, 9, 5], [9, 4, 9, 8, 4])); // [4,9]
