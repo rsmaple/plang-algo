e9b29c81862090bbb022c33d59266d6dae014031
rsmaple <wogkdkrm@gmail.com>
2026-02-22 22:51:54 +0900

    feat: sol_1 - 괄효 유효성 검사

--- src/sol_1/solution.ts
+++ src/sol_1/solution.ts
@@ -1,1 +1,27 @@
-export function solution() {}
+export function solution(s: string) {
+  const stack: string[] = [];
+
+  for (let char of s) {
+    if (char === '(' || char === '{' || char === '[') {
+      stack.push(char);
+    } else {
+      const lastChar = stack.pop();
+
+      if (
+        (char === ')' && lastChar !== '(') ||
+        (char === '}' && lastChar !== '{') ||
+        (char === ']' && lastChar !== '[')
+      ) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(solution('()')); // true
+console.log(solution('()[]{}')); // true
+console.log(solution('(]')); // false
+console.log(solution('([)]')); // false
+console.log(solution('{[]}')); // true
