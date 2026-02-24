d9120cd0e47d07fc0bcbe8a2a6a36aa79c0bd0ec
rsmaple <wogkdkrm@gmail.com>
2026-02-24 23:22:23 +0900

    feat: sol_2 기본 예시 test case

--- src/sol_2/solution.test.ts
+++ src/sol_2/solution.test.ts
@@ -1,7 +1,16 @@
-import { describe, expect, it } from "vitest";
-import { solution } from "./solution";
+import { describe, expect, it } from 'vitest';
+import { solution } from './solution';
 
-describe("sol_2", () => {
-  it("Example 1", () => {
+describe('sol_2', () => {
+  it('"abcabcbb" -> 3', () => {
+    expect(solution('abcabcbb')).toBe(3);
+  });
+
+  it('"bbbbb" -> 1', () => {
+    expect(solution('bbbbb')).toBe(1);
+  });
+
+  it('"pwwkew" -> 3', () => {
+    expect(solution('pwwkew')).toBe(3);
   });
 });
