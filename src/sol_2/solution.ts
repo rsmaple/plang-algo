cd37fb09d50f89267861dd021a04950a89926fe2
rsmaple <wogkdkrm@gmail.com>
2026-02-24 23:15:42 +0900

    feat: sol_2 - 슬라이딩 윈도우

--- src/sol_2/solution.ts
+++ src/sol_2/solution.ts
@@ -1,2 +1,23 @@
-export function solution() {
+export function solution(s: string) {
+  const charSet = new Set<string>();
+
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (charSet.has(s[right])) {
+      charSet.delete(s[left]);
+      left++;
+    }
+
+    charSet.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
 }
+
+console.log(solution('abcabcbb')); // 3 ("abc")
+console.log(solution('bbbbb')); // 1 ("b")
+console.log(solution('pwwkew')); // 3 ("wke")
+console.log(solution('')); // 0
